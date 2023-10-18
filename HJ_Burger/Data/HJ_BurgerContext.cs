1b43d1a85db39e8b30efeb82b25d3d83381a55d4
Hernan Jurado <Hernan.jurado@udla.edu.ec>
2023-10-18 08:38:15 -0500

    Creacion Burger

---
+++ HJ_Burger/Data/HJ_BurgerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HJ_Burger.Models;
+
+namespace HJ_Burger.Data
+{
+    public class HJ_BurgerContext : DbContext
+    {
+        public HJ_BurgerContext (DbContextOptions<HJ_BurgerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HJ_Burger.Models.Burger> Burger { get; set; } = default!;
+    }
+}
